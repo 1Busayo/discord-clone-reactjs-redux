403dfecfbb757f194d5ba046d8b985f6a719c59d
unknown <fanimokunbusayo@gmail.com>
2020-12-20 00:26:18 +0100

    reating Sidebars

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets build a discord clone</h1>
+    <div className="app">
+     
+                {/* sidebar */}
+                <Sidebar />
+                {/* Chat */}
+
     </div>
   );
 }
