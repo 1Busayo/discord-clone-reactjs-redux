c883cf01a419f8bb97bc4cc0bbe88482dfbaf42f
unknown <fanimokunbusayo@gmail.com>
2020-12-20 02:36:59 +0100

    sidebar icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,14 @@ import React from 'react';
 import "./Sidebar.css";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
+import {Avatar} from '@material-ui/core';
+import MicIcon from '@material-ui/icons/Mic';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 function Sidebar() {
     return (
@@ -13,7 +21,7 @@ function Sidebar() {
   </div>
   <div className="sidebar__channels">
       <div className="sidebar__channelsHeader">
-          <div className="sideabr__header">
+          <div className="sidebar__header">
               <ExpandMoreIcon />
               <h4>Text Channels</h4>
           </div>
@@ -21,7 +29,42 @@ function Sidebar() {
 <AddIcon className="sidebar__addChannel" />
           
       </div>
-  </div>
+<div className="sidebar__channelsList">
+    <SidebarChannel />
+    <SidebarChannel />
+    <SidebarChannel />
+    <SidebarChannel />
+</div>  
+</div>
+
+<div className="sidebar__voice">
+    <SignalCellularAltIcon className="sidebar__voiceIcon" fontSize="large" />
+    <div className="sidebar__voiceInfo">
+<h3>Voice connected</h3>
+<p>Stream</p>
+
+    </div>
+
+<div className="sidebar__voiceIcons">
+    <InfoOutlinedIcon />
+    <CallIcon />
+</div>
+
+
+</div>
+<div className="sidebar__profile">
+    <Avatar />
+    <div className="sidebar__profileInfo">
+      <h3>@samuel</h3>
+      <p>#thisIsMyID</p>  
+    </div>
+<div className="sidebar__profileIcons">
+    <MicIcon />
+    <HeadsetIcon />
+    <SettingsIcon />
+</div>
+
+</div>
         </div>
     )
 }
